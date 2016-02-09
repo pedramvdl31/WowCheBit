b72755318868e2669b416bfb646dc98dfe0d8680
pedram <pedram@eyelevate.com>
2016-02-09 05:51:42 -0700

    'new-feture-percentage'

--- resources/views/admins/index.blade.php
+++ resources/views/admins/index.blade.php
@@ -17,28 +17,4 @@
 	    </div>
 	@endif
 
-
-	<div class=" col-md-4 col-sm-6 col-xs-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">Flag Notification</div>
-			<div class="panel-body">
-				Basic panel example
-			</div>
-			<div class="panel-footer clearfix">
-				<button class='btn btn-primary btn-sm pull-right'> View </button>
-			</div>
-		</div>
-	</div>
-	<div class=" col-md-4 col-sm-6 col-xs-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">Flag Notification</div>
-			<div class="panel-body">
-				Basic panel example
-			</div>
-			<div class="panel-footer clearfix">
-				<button class='btn btn-primary btn-sm pull-right'> View </button>
-			</div>
-		</div>
-	</div>
-
 @stop
