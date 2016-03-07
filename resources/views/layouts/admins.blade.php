b7b649561f1c44b2ffe8664e2f528fed63071f9d
pedram <pedram@eyelevate.com>
2016-03-07 01:24:52 -0700

    'acl-hide'

--- resources/views/layouts/admins.blade.php
+++ resources/views/layouts/admins.blade.php
@@ -85,7 +85,7 @@
                                         </li>
                                     </ul>
                                 </li> -->
-                                <li class="li-menus"><a class=""><i class="fa fa-edit"></i> Control Panel <span class="fa fa-chevron-down"></span></a>
+                                <li class="li-menus first-li-m"><a class="first-li-a"><i class="fa fa-edit"></i> Control Panel <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu" style="display: none">
                                         <li>Buy/Sell List</li>
                                         <li class="navLi"><a href="{!! route('buysells_index') !!}">View All</a>
