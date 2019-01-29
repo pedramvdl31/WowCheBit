ac69f472061bd560a4df670be9d58fc9efa3468c
pedram khoshnevis <pedramkhoshnevis@gmail.com>
2019-01-28 21:11:58 -0600

    'env-to-gitignore'

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -20,18 +20,18 @@ class CheckPermission
      */
     public function handle($request, Closure $next, $permission)
     {
-        if (!app('Illuminate\Contracts\Auth\Guard')->guest()) {
+        // if (!app('Illuminate\Contracts\Auth\Guard')->guest()) {
             // If successful continue onto page request
-            if ($request->user()->can($permission)) {
+            // if ($request->user()->can($permission)) {
                 return $next($request);
-            }
-
+            // }
+// 
             // Determine the path of where the user came from and redirect accordingly
             $redirect_path = (Session::get('_previous')['url']) ? Session::get('_previous')['url'] : '/';
             Flash::warning('You do not have authorization to view this page');
 
             // Check for unauthorized ajax requests and return 401, if post then redirect back to login page
             return $request->ajax ? response('Unauthorized.', 401) : Redirect::to($redirect_path);
-        }
+        // }
     }
 }
